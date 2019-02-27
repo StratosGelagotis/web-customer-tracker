c3523d2fd727d3c264bb1df0612a73c1b4aabf5a
StratosGelagotis <venetoulis@gmail.com>
2019-02-27 21:33:28 +0200

    Initial Commit

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,29 @@
 package com.luv2code.springdemo.controller;
 
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-    @RequestMapping("/list")
-    public String listCustomers(Model theModel){
+    // need to inject the customer service
+    @Autowired
+    private CustomerService customerService;
 
+    @GetMapping("/list")
+    public String listCustomers(Model theModel){
+        // get the customers from the CustomerService
+        List<Customer> theCustomers = customerService.getCustomers();
+        // add the customers to the model
+        theModel.addAttribute("customers",theCustomers);
         return "list-customers";
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -5,14 +5,4 @@
   Time: 7:52 PM
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-    <a href="TestDbServlet">Test Db Servlet</a>
-    <br><br>
-    <a href="customer/list">List Customers</a>
-  </body>
-</html>
+<% response.sendRedirect("customer/list"); %>
